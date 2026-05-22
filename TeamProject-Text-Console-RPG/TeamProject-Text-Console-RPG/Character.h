46984d6aa3592675c187524cac3bd21aa24678a3
Melo <melo1011@naver.com>
2026-05-22 20:05:04 +0900

    메인 불러오기용
    1

--- TeamProject-Text-Console-RPG/TeamProject-Text-Console-RPG/Character.h
+++ TeamProject-Text-Console-RPG/TeamProject-Text-Console-RPG/Character.h
@@ -5,6 +5,7 @@
 #include<string>
 #include<vector>
 
+#include "Monster.h"
 #include "Item.h"
 
 class Character
@@ -13,11 +14,16 @@ private:
     static Character* instance;
 
     std::string name;
-    int level;
-    int hp;
-    int maxhp;
-    int attack;
-    int experience;
+    std::string job = "무직";
+    std::string skillname;
+    
+    int level = 1 ;
+    int hp = 200 ; // 현재체력 
+    int maxhp = 200 ; // 최대체력 
+    int mp = 100; // 현재마나 
+    int maxmp = 100; // 최대마나 
+    int attack = 30 ;
+    int exp = 0;
     int gold;
 
     std::vector<Item*> inventory;
@@ -27,7 +33,35 @@ public:
 
     static Character* getInstance(std::string name = "");
 
-    void displayStatus();
-    void levelUp();
-    void useItem(int index);
+    void Displaystatus();
+    void Levelup();
+    void Useitem(int index);
+    bool Skill(Monster* monster);
+    void Additem(string item);
+        
+    virtual string Attackmessage() = 0;
+    virtual bool Skill (Monster* monster) = 0;
+    virtual void Basicattack(Monster* monster) = 0;
+
+    //setter 설정
+    void Setjob(std::string job) { this->job = job; }
+    void Setlevel(int level) { this->level = level; }
+    void Sethp(int hp) { this->hp = hp; }
+    void Sethp(int maxhp) { this->maxhp = hp; }
+    void Setmp(int mp) { this->mp = mp; }
+    void Setmp(int maxmp) { this->maxmp = mp; }
+    void Setattack(int attack) { this->attack = attack; }
+    void Setexp(int exp) { this->exp = exp; }
+    void Setgold(int gold) { this->gold = gold; }
+
+    //getter 설정
+    string Getjob() { return job; }
+    int Getlevel() { return level; }
+    int Gethp() { return hp; }
+    int Getmaxhp() { return maxhp; }
+    int Getmp() { return mp; }
+    int Getmaxmp() { return maxmp; }
+    int Getattack() { return attack; }
+    int Getexp() { return exp; }
+    int Getgold() { return gold; }
 };
